247a55fae04ef69e49653687988c25a386ac73ff
Gregory Rose <greg@leanplum.com>
2014-01-17 22:07:09 -0500

    integrating FB status

--- Spontaneity/Spontaneity/AppDelegate.h
+++ Spontaneity/Spontaneity/AppDelegate.h
@@ -9,8 +9,15 @@
 #import <UIKit/UIKit.h>
 #import <FacebookSDK/FacebookSDK.h>
 
+#import "LoginViewController.h"
+
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
 
 @property (strong, nonatomic) UIWindow *window;
+@property (strong, nonatomic) LoginViewController *loginViewController;
+
+- (void)sessionStateChanged:(FBSession *)session state:(FBSessionState) state error:(NSError *)error;
+- (void)userLoggedIn;
+- (void)userLoggedOut;
 
 @end
--- Spontaneity/Spontaneity/LoginViewController.m
+++ Spontaneity/Spontaneity/LoginViewController.m
@@ -7,10 +7,13 @@
 //  View Controller for App Launch and Facebook Login
 //
 
+#import <FacebookSDK/FacebookSDK.h>
+
 #import "LoginViewController.h"
+#import "AppDelegate.h"
 
 @interface LoginViewController ()
-
+- (IBAction)buttonClicked:(id)sender;
 @end
 
 @implementation LoginViewController
@@ -34,6 +37,33 @@
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)buttonClicked:(id)sender
+{
+    // If the session state is any of the two "open" states when the button is clicked
+    if (FBSession.activeSession.state == FBSessionStateOpen
+        || FBSession.activeSession.state == FBSessionStateOpenTokenExtended) {
+        
+        // Close the session and remove the access token from the cache
+        // The session state handler (in the app delegate) will be called automatically
+        [FBSession.activeSession closeAndClearTokenInformation];
+        
+        // If the session state is not any of the two "open" states when the button is clicked
+    } else {
+        // Open a session showing the user the login UI
+        // You must ALWAYS ask for basic_info permissions when opening a session
+        [FBSession openActiveSessionWithReadPermissions:@[@"basic_info"]
+                                           allowLoginUI:YES
+                                      completionHandler:
+         ^(FBSession *session, FBSessionState state, NSError *error) {
+             
+             // Retrieve the app delegate
+             AppDelegate* appDelegate = [UIApplication sharedApplication].delegate;
+             // Call the app delegate's sessionStateChanged:state:error method to handle session state changes
+             [appDelegate sessionStateChanged:session state:state error:error];
+         }];
+    }
 }
 
 @end
