b8c61e235e111ece08ad22cdd5e88e506a5984ef
Angela Zhang <angelafzhang@gmail.com>
2014-01-18 07:48:10 -0500

    s

--- Spontaneity/Spontaneity/EventListViewController.m
+++ Spontaneity/Spontaneity/EventListViewController.m
@@ -186,6 +186,10 @@
 {
     NSString* eventKey = [self.eventKeys objectAtIndex:indexPath.row];
     NSLog(@"Selected event: %@", eventKey);
+    NSMutableDictionary *event = self.events[eventKey];
+    for (NSString *key in event) {
+        NSLog(@"%@, %@", key, [event objectForKey:key]);
+    }
     // TODO: Navigate to event detail controller
 }
 
